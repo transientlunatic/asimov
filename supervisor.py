c708d4ef523b67d48b393ba19df4e715da25f431
Daniel Williams <daniel.williams@ligo.org>
2020-01-30 11:42:56 -0800

    Updated the supervisor script.

--- supervisor.py
+++ supervisor.py
@@ -14,22 +14,141 @@ import gitlab
 import json
 import time
 import os
+import glob
 
 import htcondor
 
+import subprocess
+from configparser import ConfigParser
+
+import re
+
+import datetime
+
 access_token = os.environ['GITLAB_TOKEN'] # 
 project_id = 3816
-uber_repo = "" # The repository which contains all of the submodularised event repositories.
+uber_repo = "/home/daniel.williams/events/O3/o3a_catalog_events" # The repository which contains all of the submodularised event repositories.
 
 gl = gitlab.Gitlab('https://git.ligo.org', private_token=access_token)
 pe_catalogue = gl.projects.get(project_id)
 
-with open("pe-robot.json", "r") as data:
-    runs = json.load(data)
+#with open("pe-robot.json", "r") as data:
+#    runs = json.load(data)
+
+runs = {}
 
 schedd_ad = htcondor.Collector().locate(htcondor.DaemonTypes.Schedd)
 schedd = htcondor.Schedd(schedd_ad)
 
+def fix_ini_caps(ini):
+
+    ini.set("condor", "mergeNSscript", ini.get("condor", "mergensscript"))
+    ini.set("condor", "mergeMCMCscript", ini.get("condor", "mergemcmcscript"))
+    ini.set("condor", "combinePTMCMCh5script", ini.get("condor", "combineptmcmch5script"))
+    return ini
+
+def get_psds(rundir):
+    dets = ['H1', 'L1', 'V1']
+    files = []
+    for det in dets:
+        psd_dir = "ROQdata/0/BayesWave_PSD_{det}/post/clean/glitch_median_PSD_forLI_{det}.dat"
+        psd_file = os.path.join(rundir, psd_dir)
+        exists = os.path.exists(psd_file)
+        if exists:
+            files.append(psd_files)
+    return files
+
+def check_for_psd(rundir):
+
+    files = get_psds(rundir)
+    if len(files) >=1:
+        return True
+    else:
+        return False
+
+def check_for_ini(event_dir, event):
+    c01_dir = os.path.join(event_dir, "C01_offline")
+    ini = glob.glob(os.path.join(c01_dir, "*.ini"))
+    return ini
+
+def start_run(event):
+    event_repo = os.path.join(uber_repo, event)
+    if not os.path.exists(event_repo):
+        print("++ I can't find the repository for this event.")
+
+    ini_files = check_for_ini(event_repo, event)
+
+    if not len(ini_files)>=2:
+        print("++ I can't find the all the ini files")
+
+
+    c01_dir = os.path.join(event_repo, "C01_offline")
+
+    os.chdir(c01_dir)
+
+    gps_file = f"{event}_gpsTime.txt"
+    if not os.path.exists(os.path.join(c01_dir, gps_file)):
+        gps_file = f"{event}_gps_time.txt"
+
+    for filename in ini_files:
+        if "Prod0" in filename:
+            prod0_ini = filename
+    
+    # Update the ini file
+    P = ConfigParser()
+    P.optionxform=str
+    P.read(prod0_ini)
+
+    P.set("paths", "webdir", os.path.join("/home/daniel.williams/public_html/LVC/projects/O3/C01/", event))
+
+    P = fix_ini_caps(P)
+
+    with open(prod0_ini,'w') as fp:
+        P.write(fp)
+
+
+    pipe_ret = subprocess.call(["lalinference_pipe", "-g", f"{gps_file}", "-r", "Prod0", f"{prod0_ini}"])
+
+    if not pipe_ret == 0:
+        # This job has failed
+        return None
+
+    dagman = subprocess.Popen(["condor_submit_dag", os.path.join(event_repo, "C01_offline", "Prod0", "multidag.dag")], stdout=subprocess.PIPE, 
+            stderr=subprocess.STDOUT   )
+    stdout,stderr = dagman.communicate()
+
+    cluster = re.search("submitted to cluster ([\d]+)", str(stdout)).groups()[0]
+
+    return cluster
+
+def get_rundir_from_condor(job_data):
+    error_file = job_data['Err']
+    rundir = "/".join(error_file.split("/")[:-1])
+    return rundir
+
+def get_condor_status(cluster_id):
+    data = {}
+    for schedd_ad in htcondor.Collector().locateAll(htcondor.DaemonTypes.Schedd):
+        schedd = htcondor.Schedd(schedd_ad)
+        jobs = schedd.xquery(requirements="ClusterId == {}".format(cluster_id))
+        for job in jobs:
+            data = job
+    return data
+
+def get_run_info(issue):
+    notes = issue.notes
+    data = {}
+    for note in notes.list():
+        if "# Run information" in note.body:
+            rows = note.body.split('\n')[2:]
+            
+            for row in rows:
+                try:
+                    key, value = row.split(":")
+                    data[key.strip()] = value.strip()
+                except:
+                    pass
+    return data
 
 if __name__ == "__main__":
     while True:
@@ -43,30 +162,88 @@ if __name__ == "__main__":
                 print(f("I need to start {issue.title}"))
             elif "C01::Prod0 Running" in issue.labels:
                 try:
-                    cluster_id = runs[issue.title]['cluster_id']
+                    cluster_id = runs[issue.title]['Prod0_cluster_id']
                 except:
-                    cluster_id = None
-                    print(f"I need to know the cluster ID for {issue.title}")
+                    data = get_run_info(issue)
+                    if 'Prod0' in data.keys():
+                        cluster_id = data['Prod0']
+                    else:
+                        cluster_id = None
+                        issue.labels += ["C01 cluster missing"]
 
                 if cluster_id:
-                    jobs = schedd.xquery(requirements="ClusterId == {}".format(cluster_id))
-                    for job in jobs:
-                        if job['JobStatus'] == 2:
-                            # The job is still running
-                            print(f"* {issue.title} is running.")
-                        elif job['JobStatus'] == 5:
-                            # The job is held.
-                            # Mark it as stuck
-                            issue.labels = issue.labels + ["C01::Stuck"]
-                            issue.notes.create({'body': 'This job was held at {}'.format(time.now())})
+                    job = get_condor_status(cluster_id)
+                    if not 'JobStatus' in job:
+                        print(f"Something's wrong with {issue.title}")
+                        issue.labels = issue.labels + ["C01::Stuck"]
+                        issue.notes.create({'body': '# Run problem\nThe Prod0 job ran into an unknown error at {}'.format(datetime.datetime.now())})
+                        
+                        continue
+                    elif job['JobStatus'] == 2:
+                        # The job is still running
+                        #print(f"* {issue.title} is running.")
+                        pass
+                    elif job['JobStatus'] == 4:
+                        # The job has completed
+                        issue.labels += ["C01 Prod0 run complete"]
+                    elif job['JobStatus'] == 5:
+                        # The job is held.
+                        # Mark it as stuck
+                        issue.labels = issue.labels + ["C01::Stuck"]
+                        issue.notes.create({'body': '# Job hold\nThe Prod0 job was held at {}'.format(datetime.datetime.now())})
 
-                print(f"I need to check if the PSDs are ready for {issue.title}")
+                if not "PSD Ready" in issue.labels:
+
+                    #print(f"I need to check if the PSDs are ready for {issue.title}")
+                    if cluster_id:
+                        rundir = get_rundir_from_condor(job)
+                        ready = check_for_psd(rundir)
+                        psds = get_psds(rundir)
+                        #print(f"The rundir is {rundir}")
+                        if ready:
+                            print(f"The PSD files for {issue.title} are ready.")
+                            issue.labels += ["PSD ready"]
+                            
+                            issue.notes.create({'body': '# PSD Files\nThe PSD files for this event have been created. Remember to update the ini file for Prod1 to include these.\nThe PSDs are located at {psds}.'})
+
             elif "C01::Prod1 Running" in issue.labels:
-                print(f"I need to check if the jobs are finished for {issue.title}")
-            else:
-                print(f"I need to check if {issue.title} is running")
+                try:
+                    cluster_id = runs[issue.title]['Prod1_cluster_id']
+                except:
+                    data = get_run_info(issue)
+                    if 'Prod1' in data.keys():
+                        cluster_id = data['Prod1']
+                    else:
+                        cluster_id = None
+                        issue.labels += ["C01 cluster missing"]
+                        
 
-        with open("pe-robot.json", "w") as data:
-            json.dump(runs, data)
+                if cluster_id:
+                    job = get_condor_status(cluster_id)
+                    if not 'JobStatus' in job:
+                        print(f"Something's weird with Prod1 for {issue.title}")
+                        issue.labels +=  ["C01::Stuck"]
+                        issue.notes.create({'body': '# Run problem\nThe Prod1 job ran into an unknown error at {}'.format(datetime.datetime.now())})
+                    elif job['JobStatus'] == 2:
+                        # The job is still running
+                        pass
+                    elif job['JobStatus'] == 4:
+                        # The job has completed
+                        issue.labels += ["C01 Prod1 run complete"]
+
+                #print(f"I need to check if the jobs are finished for {issue.title}")
+
+            elif "C01::Ready to start" in issue.labels:
+                print(f"* {issue.title} is marked ready to start.")
+                cluster = start_run(issue.title)
+                print(f"Submitted to cluster {cluster}")
+                if cluster:
+                    issue.labels += ["C01::Prod0 Running"]
+                    issue.notes.create({'body': f"# Run information\n\nProd0: {cluster}"})
+                    issue.assignee_ids = [246]
+            issue.save()
+
+        # with open("pe-robot.json", "w") as data:
+        #     json.dump(runs, data)
 
         time.sleep(300)
