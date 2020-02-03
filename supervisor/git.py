7d042c0e3e000a1a92332fed77d2c258b1d28657
Daniel Williams <mail@daniel-williams.co.uk>
2020-02-03 21:17:40 +0000

    Updated the git module.

--- supervisor/git.py
+++ supervisor/git.py
@@ -9,7 +9,7 @@ class MetaRepository(object):
         self.directory = directory
 
         repos_list = glob.glob("*")
-        self.repos = {event, os.path.join(directory, event) for event in repos_list}
+        self.repos = {event: os.path.join(directory, event) for event in repos_list}
 
     def get_repo(self, event):
         return EventRepo(self.repos[event])
@@ -18,4 +18,11 @@ class MetaRepository(object):
 
 
 class EventRepo(object):
-    pass
+
+    def __init__(self, directory):
+
+        self.repo = git.Repo(directory)
+    
+    def find_prods(self, category="C01_offline"):
+        prods = glob.glob(os.path.join(directory, category, "Prod*.ini"))
+        return prods
