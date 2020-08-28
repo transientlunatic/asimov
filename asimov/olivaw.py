4b6b15559535fbbaf9afcdb0546a194cb64a236a
Daniel Williams <mail@daniel-williams.co.uk>
2020-08-28 17:35:19 +0100

    Added containerisation.

--- asimov/olivaw.py
+++ asimov/olivaw.py
@@ -1,32 +1,31 @@
 import ast
 import os, glob, datetime
+import click
+
+@click.command()
+def main():
+    """
+
+    """
+
+    from asimov import gitlab
+    from asimov import config
+    from asimov import condor, git
+    from asimov.ini import RunConfiguration
 
 
-if __name__ == "__main__":
+    import otter
 
-    @click.command()
-    def main(eventflag):
-        """
+    print("Running olivaw")
 
-        """
-        
-        from asimov import gitlab, mattermost
-        from asimov import config
-        from asimov import condor, git
-        from asimov.ini import RunConfiguration
+    server = gitlab.gitlab.Gitlab('https://git.ligo.org', private_token=config.get("gitlab", "token"))
+    repository = server.projects.get(config.get("olivaw", "tracking_repository"))
 
-        import click
+    events = gitlab.find_events(repository, milestone=config.get("olivaw", "milestone"))
 
-        import otter
-
-        server = gitlab.gitlab.Gitlab('https://git.ligo.org', private_token=config.get("gitlab", "token"))
-        repository = server.projects.get(config.get("olivaw", "tracking_repository"))
-
-        events = gitlab.find_events(repository, milestone=config.get("olivaw", "milestone"))
-
-        for event in events:
-            print(event.title)
-            print ("----------")
-            print(event.meta)
-            print("-----------")
-            print(event.get_all_latest())
+    for event in events:
+        print(event.title)
+        print ("----------")
+        print(event.meta)
+        print("-----------")
+        print(event.get_all_latest())
