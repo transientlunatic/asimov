ebaddc67835222fbb3330213c78ee200a049f896
Daniel Williams <mail@daniel-williams.co.uk>
2020-09-16 17:02:12 +0100

    Added logging object.

---
+++ asimov/logging.py
@@ -0,0 +1,101 @@
+"""
+This module contains tools to log data from runs.
+Unlike a conventional python process, processes run by asimov may wish to send information to a number of different locations, and store
+information for review purposes.
+"""
+
+import logging
+import mattermost
+
+from asimov import config
+from .gitlab import EventIssue
+
+class AsimovLogger():
+
+    def __init__(self, event):
+        """
+        An object for logging asimov productions.
+
+        Parameters
+        ----------
+        event : ``asimov.event.Event``
+           The event which this logger is for.
+        """
+
+        self.event_object = event
+        self.event_issue = event.issue_object
+        self.mattermost = mattermost.Mattermost()
+        self.log_file = f"{event.work_dir}/asimov.log"
+        self.file_logger = logging.basicConfig(filename=self.log_file,level=logging.DEBUG)
+        
+    def log(self, level, message, production=None, channels="file"):
+        """
+        Record a log message.
+
+        Parameters
+        ----------
+        level : str, {DEBUG, INFO, WARNING, ERROR}
+           The level of the logging message.
+        production : ``asimov.event.Production``, optional
+           The production this message is for.
+        message : str
+           The free-form message for the log message.
+        channels : list, or str, {"file", "mattermost", "gitlab"}, optional
+           The list of places where the log message should be sent.
+           Defaults to file only.
+        """
+
+        logger_levels = {"debug": logger.DEBUG,
+                         "info": logger.INFO,
+                         "warning": logger.WARNING,
+                         "error": logger.ERROR}
+
+        if not production:
+            production = ""
+        
+        if "file" in channels:
+            self.file_logger.log(logger_levels[level.lower()],
+                                 message)
+
+        if "mattermost" in channels:
+            self.mattermost.send_message(f":mega: {self.event_object.name} {production} {message} :robot:}")
+
+        if "gitlab" in channes:
+            self.event_issue.add_note(message)
+            
+        
+    def info(self, message, production=None, channels="file"):
+        """
+        Record an information message.
+
+        Parameters
+        ----------
+
+        production : ``asimov.event.Production``, optional
+           The production this message is for.
+        message : str
+           The free-form message for the log message.
+        channels : list, or str, {"file", "mattermost", "gitlab"}, optional
+           The list of places where the log message should be sent.
+           Defaults to file only.
+        """
+
+        self.log("info", message, production, channels)
+
+    def error(self, message, production=None, channels="file"):
+        """
+        Record an error message.
+
+        Parameters
+        ----------
+
+        production : ``asimov.event.Production``, optional
+           The production this message is for.
+        message : str
+           The free-form message for the log message.
+        channels : list, or str, {"file", "mattermost", "gitlab"}, optional
+           The list of places where the log message should be sent.
+           Defaults to file only.
+        """
+
+        self.log("error", message, production, channels)
