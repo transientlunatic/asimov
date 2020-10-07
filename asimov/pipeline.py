3952602f3206b8c1297a350784da570e5c04688a
Richard Philip Udall <richard.udall@ldas-pcdev6.ligo.caltech.edu>
2020-10-07 09:06:46 -0700

    added alternate logic so that the correct dag is submitted for RIFT

--- asimov/pipeline.py
+++ asimov/pipeline.py
@@ -168,7 +168,11 @@ class Pipeline():
         self.before_submit()
         
         try:
-            command = ["condor_submit_dag",
+            if self.production.lower() == "rift":
+                command = ["condor_submit_dag",
+                                   os.path.join(self.production.rundir, "marginalize_intrinsic_parameters_BasicIterationWorkflow.dag")]
+            else:
+                command = ["condor_submit_dag",
                                    os.path.join(self.production.rundir, "multidag.dag")]
             dagman = subprocess.Popen(command,
                                   stdout=subprocess.PIPE,
