ec73dd75a2cb6b52105e0e8c9e5b6b3422f71658
Daniel Williams <daniel.williams@glasgow.ac.uk>
2021-05-20 14:38:19 +0100

    Added DAG generation tests.!

--- tests/integration/test_gwtc2d1.py
+++ tests/integration/test_gwtc2d1.py
@@ -4,8 +4,10 @@ ini and DAG creation.
 """
 
 import unittest
+import os
 
 import asimov.event
+from asimov.cli import known_pipelines
 
 
 
@@ -18,16 +20,29 @@ class EventTests(unittest.TestCase):
     def setUp(self):
         with open("GW190426190642.yaml", "r") as f:
             TEST_YAML = f.read()
+        TEST_YAML = TEST_YAML.format(wd=os.getcwd())
         self.event = asimov.event.Event.from_yaml(TEST_YAML, repo=False)
 
     def test_ini_generation(self):
         """Check that the ini file is correctly generated."""
 
         for production in self.event.productions:
-            print(f"Production {production.name}")
             production.make_config(f"{self.event.name}_{production.name}.ini",
                                    template_directory="test_templates")
 
+    def test_commandline(self):
+        """Check that the correct commandline is run."""
+        for production in self.event.productions:
+            production.make_config(f"{production.name}.ini",
+                                   template_directory="test_templates")
+            if production.pipeline.lower() in known_pipelines:
+                try:
+                    pipe = known_pipelines[production.pipeline.lower()](production, "C01_offline")
+                    pipe.clean()
+                    pipe.build_dag()
+                except Exception as e:
+                    print(production.name)
+                    print(e)
 
 if __name__ == '__main__':
     unittest.main()
