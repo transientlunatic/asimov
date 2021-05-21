56ae7e6c59f7a49062f92493231fb6b6e3f30f0a
Daniel Williams <mail@daniel-williams.co.uk>
2021-05-21 14:43:01 +0100

    Added multiprocessing.

--- asimov/pipeline.py
+++ asimov/pipeline.py
@@ -148,6 +148,7 @@ class Pipeline():
             "--redshift_method", config.get('pesummary', 'redshift'),
             "--nsamples_for_skymap", config.get('pesummary', 'skymap_samples'),
             "--evolve_spins", "True",
+            "--multi_process", "6",
             "--config", os.path.join(self.production.event.repository.directory, self.category, configfile)]
         # Samples
         command += ["--samples"]
