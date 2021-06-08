11d5959fbc0ffd6fa0a091e55973ab4b37927673
Daniel Williams <mail@daniel-williams.co.uk>
2021-06-08 15:03:09 +0100

    Updates

--- asimov/event.py
+++ asimov/event.py
@@ -1011,7 +1011,7 @@ class Production:
             raise ValueError("This isn't a valid ini file")
 
         return ini
-
+    
     @classmethod
     def from_dict(cls, parameters, event, issue=None):
         name, pars = list(parameters.items())[0]
