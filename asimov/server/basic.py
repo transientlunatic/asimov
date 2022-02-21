23ac7c16c5d24af47daa98350a2829633e6415f7
Daniel Williams <mail@daniel-williams.co.uk>
2022-02-21 11:37:47 +0000

    Moved around the examples to live with the code.

--- asimov/server/basic.py
+++ asimov/server/basic.py
@@ -14,10 +14,44 @@ bp = Blueprint("asimov", __name__, url_prefix="")
 
 @bp.route("/", methods=['GET'])
 def splash():
+    """
+    The root endpoint for asimov.
+
+    Example
+    -------
+
+    .. http:example:: curl wget python-requests
+
+       GET / HTTP/1.1
+       Content-Type: application/json
+
+
+       HTTP/1.1 200 OK
+       Content-Type: application/json
+
+       "asimov"
+    """
     return make_response(jsonify(asimov.__name__), 200)
 
 @bp.route("/version", methods=['GET'])
 def version():
+    """
+    Show the version information for asimov and the pipelines it is designed to run.
+
+    Example
+    -------
+
+    .. http:example:: curl wget python-requests
+
+       GET /version HTTP/1.1
+       Content-Type: application/json
+
+
+       HTTP/1.1 200 OK
+       Content-Type: application/json
+
+       "v0.3.4"
+    """
     return make_response(jsonify(asimov.__version__), 200)
 
 
