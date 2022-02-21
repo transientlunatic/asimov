f7a04ae4c27f21ca9b6cedaf214886d0e5e8d9de
Daniel Williams <mail@daniel-williams.co.uk>
2022-02-21 13:22:18 +0000

    Update the logging interface to use blueprints.

--- asimov/server/__init__.py
+++ asimov/server/__init__.py
@@ -4,67 +4,44 @@ The asimov server
 This feature allows asimov to monitor and interact with ongoing
 analyses without the need for an ongoing cronjob.
 """
-import datetime
-
-from flask import jsonify, make_response
-
-
 from flask import Flask
-from flask import request
 
-from asimov.server import database
-app = Flask("asimov")
+from flask.json import JSONEncoder
+from datetime import date
 
-@app.route("/log", methods=["POST"])
-def add_logging_event():
-    """Add a new logging event to the database
+from . import basic
+from . import logging
 
-    Examples
-    --------
+class CustomJSONEncoder(JSONEncoder):
+    def default(self, obj):
+        try:
+            if isinstance(obj, date):
+                return obj.isoformat()
+            iterable = iter(obj)
+        except TypeError:
+            pass
+        else:
+            return list(iterable)
+        return JSONEncoder.default(self, obj)
 
-    .. http:example:: curl wget python-requests
 
-       POST /log HTTP/1.1
-       Content-Type: application/json
-
-       {
-         "event":"GW150914",
-         "level":"update",
-         "message":"This is a test message.",
-         "module":"sampling",
-         "pipeline":"pycbc",
-         "production":"ProdX10"
-       }
-
+def create_app():
     """
-    if request.is_json:
-        req = request.get_json()
-
-    entry = database.Logger(pipeline=req['pipeline'],
-                            module=req['module'],
-                            level=req['level'],
-                            time=datetime.datetime.now(),
-                            event=req['event'],
-                            production=req['production'],
-                            message=req['message']
-                            )
-    entry.save()
-    return make_response({}, 200)
-
-
-@app.route("/log", methods=["GET"])
-def list_logging_events():
-    """List all of the log events which match filters given as GET arguments
-
-    Examples
-    --------
-    .. http:example:: curl wget python-requests
-
-       GET /log HTTP/1.1
-       Content-Type: application/json
-
-       :query pipeline: RIFT
+    Create the web app for asimov
     """
-    filters = request.args
-    events = database.Logger.list(**filters)
-    return make_response(jsonify([event.__dictrepr__() for event in events]), 200)
+    app = Flask("asimov")
+    
+    app.json_encoder = CustomJSONEncoder
+
+    app.register_blueprint(logging.bp)
+    
+    return app
+
+
+def bootstrap():
+    app = create_app()
+    app.run(debug=False)
+
+
+if __name__ == '__main__':
+    bootstrap()
