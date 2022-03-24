e7110596b9b46cafbe931e7d0526c64dc79f6614
Daniel Williams <daniel.williams@ligo.org>
2022-03-24 07:30:00 -0700

    More refinements to the new draft condor interface.

--- scripts/condor_tests.py
+++ scripts/condor_tests.py
@@ -340,6 +340,7 @@ class CondorJobList:
 from asimov import config
 from asimov.cli import connect_gitlab, ACTIVE_STATES, known_pipelines
 from asimov import gitlab
+import yaml
 
 # server, repository = connect_gitlab()
 # events = gitlab.find_events(repository,
@@ -348,13 +349,32 @@ from asimov import gitlab
 #                             update=False,
 #                             repo=True)
 
+ledger = "/home/pe.o3/public_html/LVC/o3a-final/ledger.yaml"
+with open(ledger, "r") as filehandle:
+    events = yaml.safe_load(filehandle.read())
+
+
 jobs = CondorJobList()
 jobs.refresh()
 
-for job in jobs.jobs.values():
-    print(job)
+#for job in jobs.jobs.values():
+#    print(job)
 
-# for event in events:
+for event in events:
+    productions = {}
+    for prod in event['productions']:
+        productions.update(prod)
+
+    for name, production in productions.items():
+        if production['status'] == "running":
+            print(event['name'], name)
+            if not "job id" in production:
+                print("Job ID is missing")
+                continue
+            if production['job id'] in jobs.jobs:
+                print(jobs.jobs[production['job id']])
+            else:
+                print("\t Job is not running")
 
 #     on_deck = [production
 #                for production in event.productions
