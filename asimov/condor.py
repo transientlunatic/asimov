a9ee26b78684502e89fd79d2b56342c86e38144f
Daniel Williams <daniel.williams@glasgow.ac.uk>
2022-04-22 14:12:30 +0100

    Added documentation.

--- asimov/condor.py
+++ asimov/condor.py
@@ -7,12 +7,11 @@ In order to improve performance the code caches results from the query to the sc
 
 """
 import os
-from configparser import ConfigParser
 import yaml
 import htcondor
 import yaml
 
-from asimov import config, logger, LOGGER_LEVEL
+from asimov import config
 
 class CondorJob:
     """
@@ -34,10 +33,6 @@ class CondorJob:
            The number of hosts currently processing the job.
         status: int
            The status condition for the job.
-       
-        Examples
-        --------
-        job = CondorJob(346758)
         """
 
         self.idno = idno
@@ -58,6 +53,16 @@ class CondorJob:
     def from_dict(cls, dictionary):
         """
         Create a respresentation from a dictionary.
+
+        Parameters
+        ----------
+        dictionary : dict
+           The dictionary of job parameters.
+
+        Returns
+        -------
+        `CondorJob`
+           A condor job object.
         """
         cls = cls(idno=dictionary['id'],
                   command=dictionary['command'],
@@ -78,11 +83,24 @@ class CondorJob:
     def add_subjob(self, job):
         """
         Add a subjob of this job.
+
+        Parameters
+        ----------
+        job : `CondorJob`
+           The job which is a subjob.
         """
         self.subjobs.append(job)
         
     @property
     def status(self):
+        """
+        Get the status of the job.
+
+        Returns
+        -------
+        str
+          A description of the status of the job.
+        """
         statuses = {0: "Unexplained",
                     1: "Idle",
                     2: "Running",
@@ -96,6 +114,9 @@ class CondorJob:
 class CondorJobList:
     """
     Store the list of running condor jobs.
+
+    The list is automatically pulled from the condor scheduller if it is
+    more than 15 minutes old (by default)
     """
     def __init__(self):
         self.jobs = {}
@@ -104,12 +125,12 @@ class CondorJobList:
             self.refresh()
         else:
             age = os.stat(cache).st_mtime
-            if age > 15 * 60:
+            if age > config.get("htcondor", "cache_time"):
                 with open(cache, "r") as f:
                     self.jobs = yaml.safe_load(f)
             else:
                 self.refresh()
-        
+
     def refresh(self):
         """
         Poll the schedulers to get the list of running jobs and update the database.
@@ -119,7 +140,9 @@ class CondorJobList:
             try:
                 schedd = htcondor.Schedd(schedd_ad)
                 jobs = schedd.query(opts=htcondor.htcondor.QueryOpts.DefaultMyJobsOnly,
-                                    projection=["ClusterId", "Cmd", "CurrentHosts", "HoldReason", "JobStatus", "DAG_Status", "JobBatchName", "DAGManJobId"],
+                                    projection=["ClusterId", "Cmd", "CurrentHosts",
+                                                "HoldReason", "JobStatus", "DAG_Status",
+                                                "JobBatchName", "DAGManJobId"],
                 )
                 data += jobs
             except:
@@ -142,16 +165,15 @@ class CondorJobList:
             retdat.append(CondorJob.from_dict(job))
 
         for datum in retdat:
-            
             if not datum.dag:
                 self.jobs[datum.idno] = datum
-        # # Now search for subjobs
+                # # Now search for subjobs
         for datum in retdat:
-           if datum.dag:
-               if datum.dag in self.jobs:
-                   self.jobs[datum.dag].add_subjob(datum)
-               else:
-                   self.jobs[datum.idno] = datum
+            if datum.dag:
+                if datum.dag in self.jobs:
+                    self.jobs[datum.dag].add_subjob(datum)
+                else:
+                    self.jobs[datum.idno] = datum
 
         with open("_cache_jobs.yaml", "w") as f:
-                f.write(yaml.dump(self.jobs))
+            f.write(yaml.dump(self.jobs))
