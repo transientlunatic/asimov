84bbc602c3551da4d448421a5bdea31a1ab772d7
Daniel Williams <daniel.williams@ligo.org>
2022-04-29 21:26:30 +0100

    Update the logger for asimov management.

--- asimov/cli/manage.py
+++ asimov/cli/manage.py
@@ -7,8 +7,7 @@ import pathlib
 import click
 
 from asimov.cli import connect_gitlab, known_pipelines
-from asimov import logging
-from asimov import config
+from asimov import config, logger
 from asimov import gitlab
 from asimov.event import Event, DescriptionException, Production
 from asimov.pipeline import PipelineException
@@ -29,7 +28,6 @@ def build(event):
     events = gitlab.find_events(repository, milestone=config.get("olivaw", "milestone"), subset=[event], update=False)    
     for event in events:
         click.echo(f"Working on {event.title}")
-        logger = logging.AsimovLogger(event=event.event_object)
         ready_productions = event.event_object.get_all_latest()
         for production in ready_productions:
             click.echo(f"\tWorking on production {production.name}")
