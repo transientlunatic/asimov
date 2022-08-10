a7ec4c2c3c8da4e3430f79e4dc992728f634394b
Daniel Williams <daniel.williams@ligo.org>
2022-08-10 14:32:42 +0100

    Fixed bug which always required an event time.

--- asimov/event.py
+++ asimov/event.py
@@ -370,6 +370,18 @@ class Event:
                 data['productions'] = []
         else:
             data['productions'] = []
+
+            
+        if "interferometers" in data and "event time" in data:
+            
+            if calibration.keys() != data['interferometers']:
+                # We need to fetch the calibration data
+                from asimov.utils import find_calibrations
+                data["calibration"] = find_calibrations(data['event time'])
+        
+        if not repo and "repository" in data:
+            data.pop("repository")
+        event = cls.from_dict(data, issue=issue, update=update)
         
         if issue:
             event.issue_object = issue
