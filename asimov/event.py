02ee586a3e754c2736210a3f1a623a8c96b73ad4
Daniel Williams <daniel.williams@ligo.org>
2022-09-08 16:56:07 +0100

    Remove spurious print statement.

--- asimov/event.py
+++ asimov/event.py
@@ -872,7 +872,6 @@ class Production:
                         psds = {}    
                 except Exception as e:
                     psds = {}
-                    print(e)
         # Otherwise return no PSDs
         else:
             psds = {}
