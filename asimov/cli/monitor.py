598f027a88893156f08c3ab37cdfdc5f2000f804
Daniel Williams <daniel.williams@ligo.org>
2022-09-27 19:49:46 +0100

    Added report to the chain

--- asimov/cli/monitor.py
+++ asimov/cli/monitor.py
@@ -61,6 +61,7 @@ def stop(dry_run):
 
 
 from asimov.cli import manage
+from asimov.cli import report
 
 @click.option("--dry-run", "-n", "dry_run", is_flag=True)
 @click.command()
@@ -69,7 +70,7 @@ def start(dry_run):
 
     submit_description = {
           "executable": shutil.which("asimov"),  
-          "arguments": "monitor",
+          "arguments": "monitor --chain",
           "accounting_group": config.get("pipelines", "accounting"),
           "output": f"asimov_cron.out",
           "on_exit_remove": "false",
