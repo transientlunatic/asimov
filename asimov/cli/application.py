d3dac5e5289a59c87c1947dc4ad807e6c9156f14
Daniel Williams <daniel.williams@ligo.org>
2022-10-03 15:10:29 +0100

    Removed extra e

--- asimov/cli/application.py
+++ asimov/cli/application.py
@@ -26,7 +26,7 @@ def apply_page(file, event, ledger=ledger):
 
     quick_parse = yaml.safe_load_all(data) # Load as a dictionary so we can identify the object type it contains
 
-    for document in quick_parse:e
+    for document in quick_parse:
 
         if document['kind'] == "event":
             document.pop("kind")
