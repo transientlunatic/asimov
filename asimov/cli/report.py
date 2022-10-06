996bf270f40faa2dd4a75ddf2bcc887b8038a64c
Daniel Williams <daniel.williams@ligo.org>
2022-10-06 15:39:48 +0100

    Fixed bug with resource filename

--- asimov/cli/report.py
+++ asimov/cli/report.py
@@ -2,6 +2,9 @@
 Reporting functions
 """
 from datetime import datetime
+import pytz
+import os
+from pkg_resources import resource_string, resource_stream, resource_filename
 
 import click
 import pytz
