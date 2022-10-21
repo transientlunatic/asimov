647c761ae52bbad2bfbf39bba3208a4c8daffc59
Daniel Williams <daniel.williams@ligo.org>
2022-10-21 10:59:29 +0100

    Linting.

--- asimov/pipelines/bilby.py
+++ asimov/pipelines/bilby.py
@@ -16,7 +16,6 @@ from asimov.utils import update, set_directory
 from .. import config
 from ..pipeline import Pipeline, PipelineException, PipelineLogger, PESummaryPipeline
 
-import logging
 
 class Bilby(Pipeline):
     """
@@ -366,7 +365,7 @@ class Bilby(Pipeline):
             out += """</div>"""
 
         return out
-    
+
     def resurrect(self):
         """
         Attempt to ressurrect a failed job.
