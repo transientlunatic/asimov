21bdf48e8ffc74ff02bfff2cf6827e394af63175
Daniel Williams <daniel.williams@ligo.org>
2022-10-21 07:28:29 +0100

    Changed to using the testing PE defaults.

--- scripts/testrun.sh
+++ scripts/testrun.sh
@@ -4,7 +4,7 @@ mkdir new-project
 cd new-project
 asimov init "new project"
 
-asimov apply -f https://git.ligo.org/asimov/data/-/raw/main/defaults/production-pe.yaml
+asimov apply -f https://git.ligo.org/asimov/data/-/raw/main/defaults/testing-pe.yaml
 asimov apply -f https://git.ligo.org/asimov/data/-/raw/main/defaults/production-pe-priors.yaml
 
 asimov apply -f https://git.ligo.org/asimov/data/-/raw/main/events/gwtc-2-1/GW150914_095045.yaml
