3b26266a50e74e82e0a14bf7c58a8f28725cdd65
Daniel Williams <daniel.williams@ligo.org>
2022-10-27 17:35:16 +0000

    Resolve "Passing accouting user to pipelines"

--- asimov/cli/project.py
+++ asimov/cli/project.py
@@ -116,7 +116,12 @@ def make_project(
     default="results",
     help="The location where the results store should be created.",
 )
-def init(name, root, working="working", checkouts="checkouts", results="results"):
+@click.option(
+    "--user",
+    default=None,
+    help="The user account to be used for accounting purposes. Defaults to the current user if not set."
+)
+def init(name, root, working="working", checkouts="checkouts", results="results", user=None):
     """
     Roll-out a new project.
     """
