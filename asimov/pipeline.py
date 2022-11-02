a6eef606a5debebc0ef0bc404efb76387cdcf8b8
Daniel Williams <daniel.williams@ligo.org>
2022-11-02 17:21:07 +0000

    Attempt to fix samples issue.

--- asimov/pipeline.py
+++ asimov/pipeline.py
@@ -402,7 +402,7 @@ class PESummaryPipeline(PostPipeline):
         ]
         # Samples
         command += ["--samples"]
-        command += self.production.pipeline.samples(absolute=True)
+        command += self.production.pipeline.samples(absolute=False)
         # Calibration information
         command += ["--calibration"]
         command += calibration
