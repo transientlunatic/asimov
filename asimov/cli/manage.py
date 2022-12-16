928b98cb0c2cc99edd345d061a476fd5e3710d4e
Daniel Williams <daniel.williams@ligo.org>
2022-12-16 17:41:52 +0000

    Minor fix to the manage code to fix bug with missing ini files.

--- asimov/cli/manage.py
+++ asimov/cli/manage.py
@@ -184,7 +184,8 @@ def submit(event, update, dryrun):
                         click.style("●", fg="red")
                         + f" Unable to submit {production.name}"
                     )
-                except ValueError:
+                except ValueError as e:
+                    print("ERROR", e)
                     logger.info("Unable to submit an unbuilt production")
                     click.echo(
                         click.style("●", fg="red")
