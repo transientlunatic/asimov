b1dacfdb86b895fea5e030f8588bb73970eead13
Daniel Williams <daniel.williams@ligo.org>
2023-02-02 10:02:51 +0000

    Updated the report output for bilby.

--- asimov/pipelines/bilby.py
+++ asimov/pipelines/bilby.py
@@ -425,7 +425,37 @@ class Bilby(Pipeline):
 
     def html(self):
         """Return the HTML representation of this pipeline."""
+        pages_dir = os.path.join(self.production.event.name, self.production.name)
         out = ""
+        out += """<div class="asimov-pipeline">"""
+        out += """<ul>"""
+        out += f"""<li><a href="{pages_dir}/overview.html">Overview pages</a></li>"""
+        if self.production.status in {"finished", "uploaded"}:
+            if self.production.status == "uploaded":
+                out += f"""<li><a href="{pages_dir}/results/home.html">Summary pages</a></li>"""
+        out += """</ul>"""
+
+        image_card = """<div class="card" style="width: 18rem;">
+<img class="card-img-top" src="{0}" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">{1}</p>
+  </div>
+</div>
+        """
+
+        plots = [
+            [
+                f"{self.production.name}_1d_posterior_luminosity_distance.png",
+                "luminosity distance",
+            ],
+            [f"{self.production.name}_skymap.png", "skymap"],
+        ]
+
+        for plot in plots:
+            if self.production.status in {"finished", "uploaded"}:
+                out += image_card.format(plot[0], plot[1])
+
+        out += """</div>"""
 
         return out
 
