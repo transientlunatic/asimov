b3dc4375806038348f80e4e78ea364f9fb2ec418
Daniel Williams <daniel.williams@ligo.org>
2023-03-16 02:49:11 +0000

    Updated to ensure waveforms are written to output dict

--- scripts/testrun.sh
+++ scripts/testrun.sh
@@ -5,7 +5,7 @@ mkdir new-project-$codename
 cd new-project-$codename
 asimov init "new project $codename"
 
-asimov apply -f https://git.ligo.org/asimov/data/-/raw/main/defaults/testing-pe.yaml
+asimov apply -f https://git.ligo.org/asimov/data/-/raw/6-add-the-state-vector-to-the-analysis-defaults-files/defaults/testing-pe.yaml
 asimov apply -f https://git.ligo.org/asimov/data/-/raw/main/defaults/production-pe-priors.yaml
 
 asimov apply -f https://git.ligo.org/asimov/data/-/raw/main/events/gwtc-2-1/GW150914_095045.yaml
