5e0f8e7fed68769798554754e6b7caedfdb17e00
Daniel Williams <daniel.williams@ligo.org>
2023-04-12 11:09:03 +0100

    Update location of config file and move the condor cache.

--- asimov/cli/report.py
+++ asimov/cli/report.py
@@ -43,7 +43,7 @@ def html(event, webdir):
         author="Asimov",
         title="Asimov project report",
         theme_location=resource_filename("asimov.cli", "report-theme"),
-        config_file="asimov.conf",
+        config_file=".asimov/asimov.conf",
     )
     with report:
 
