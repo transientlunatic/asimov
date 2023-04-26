f26867f9e389f37935df8c88e272a4030560c210
Daniel Williams <daniel.williams@ligo.org>
2023-04-26 04:24:48 +0100

    Add exception catching for hooks.

--- asimov/cli/monitor.py
+++ asimov/cli/monitor.py
@@ -338,7 +338,10 @@ def monitor(ctx, event, update, dry_run, chain):
             discovered_hooks = entry_points(group="asimov.hooks.postmonitor")
             for hook in discovered_hooks:
                 if hook.name in list(ledger.data["hooks"]["postmonitor"].keys()):
-                    hook.load()(deepcopy(ledger)).run()
+                    try:
+                        hook.load()(deepcopy(ledger)).run()
+                    except Exception:
+                        pass
 
         if chain:
             ctx.invoke(report.html)
